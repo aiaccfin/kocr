8b3856ceaeee8b043f004c63fde1a74f6e4b9ac1
aiaccfin <aiaccfin@gmail.com>
2025-04-10 10:51:43 -0400

    v

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os, argparse
 from tesserat.processor import Tesserat_OCR
 
-def main():
+def tesserat():
     parser = argparse.ArgumentParser(description="Run AI OCR on a directory of documents.")
     parser.add_argument("input_dir", help="Path to the input directory containing documents")
     args = parser.parse_args()
@@ -10,5 +10,12 @@ def main():
     for file in os.listdir(args.input_dir):
         processor.process_document(os.path.join(args.input_dir, file))
 
+def openai():
+    pass
+
 if __name__ == "__main__":
-    main()
+    tesserat()
+    
+    input("AI starting... ")
+    
+    openai()
